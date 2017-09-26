a28ca7e6a39f9a89be00ac84f036992f362137b2
LDD2015 <1661280479@qq.com>
2017-09-26 16:47:33 +0800

    1

--- app/src/main/java/com/liuxj/user/MyApp.java
+++ app/src/main/java/com/liuxj/user/MyApp.java
@@ -2,9 +2,6 @@ package com.liuxj.user;
 
 import android.app.Application;
 
-import com.liuxj.user.mvvm.inject.ApplicationComponent;
-import com.liuxj.user.mvvm.inject.module.ApplicationModule;
-
 /**
  * Created by LiuXJ on 2017/9/26.
  */
@@ -13,9 +10,9 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        ApplicationComponent.Instance.init(DaggerApplicationComponent
-                .builder()
-                .applicationModule(new ApplicationModule(this))
-                .build());
+//        ApplicationComponent.Instance.init(DaggerApplicationComponent
+//                .builder()
+//                .applicationModule(new ApplicationModule(this))
+//                .build());
     }
 }
--- app/src/main/java/com/liuxj/user/base/Test.java
+++ app/src/main/java/com/liuxj/user/base/Test.java
@@ -6,4 +6,12 @@ package com.liuxj.user.base;
 
 public class Test {
     private int type;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
 }
