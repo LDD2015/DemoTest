8c21a252d0637a308198b161c66620dfaa01ac1b
LDD2015 <1661280479@qq.com>
2017-09-26 16:24:13 +0800

    1

--- app/src/main/java/com/liuxj/user/mvvm/inject/module/DataLayerModule.java
+++ app/src/main/java/com/liuxj/user/mvvm/inject/module/DataLayerModule.java
@@ -1,12 +1,20 @@
 package com.liuxj.user.mvvm.inject.module;
 
+import com.liuxj.user.mvvm.net.manager.AccountManager;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 
 /**
  * Created by LiuXJ on 2017/9/15.
  */
 @Module
 public class DataLayerModule {
-    public DataLayerModule() {
+    @Singleton
+    @Provides
+    AccountManager provideAccountManager() {
+        return new AccountManager();
     }
 }
