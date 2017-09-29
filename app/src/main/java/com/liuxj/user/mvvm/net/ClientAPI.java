905cfae94b6ca4b4533f80c5b1097ffa693658dc
LDD2015 <1661280479@qq.com>
2017-09-29 17:42:33 +0800

    update

--- app/src/main/java/com/liuxj/user/mvvm/net/ClientAPI.java
+++ app/src/main/java/com/liuxj/user/mvvm/net/ClientAPI.java
@@ -2,16 +2,17 @@ package com.liuxj.user.mvvm.net;
 
 
 import io.reactivex.Observable;
+import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 
 /**
  * Created by LiuXJ on 2017/9/15.
  */
 
-public class ClientAPI {
+public interface ClientAPI {
+
     @FormUrlEncoded
     @POST("...")
-    Observable<Result> checkPhone(
-            @Field("phone") String phone,
-            @Field("token") String token);
+    Observable<Result> doLogin();
 }
